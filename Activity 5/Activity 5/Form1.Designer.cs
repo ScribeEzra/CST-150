529526be4b3fa082bb963f35cd34e71d03901e1e
ScribeEzra <109888173+ScribeEzra@users.noreply.github.com>
2022-08-06 13:07:37 -0500

    Add files via upload

---
+++ Activity 5/Activity 5/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Activity_5
+{
+    partial class fileInputTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openInputFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.wordListLabel = new System.Windows.Forms.Label();
+            this.firstAlphabetizedLabel = new System.Windows.Forms.Label();
+            this.lastAlphabetizedLabel = new System.Windows.Forms.Label();
+            this.longestWordLabel = new System.Windows.Forms.Label();
+            this.mostVowelsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openInputFileDialog
+            // 
+            this.openInputFileDialog.FileName = "openInputFile";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(165, 12);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(151, 30);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // wordListLabel
+            // 
+            this.wordListLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wordListLabel.Location = new System.Drawing.Point(12, 65);
+            this.wordListLabel.Name = "wordListLabel";
+            this.wordListLabel.Size = new System.Drawing.Size(100, 300);
+            this.wordListLabel.TabIndex = 1;
+            this.wordListLabel.Text = "label1";
+            // 
+            // firstAlphabetizedLabel
+            // 
+            this.firstAlphabetizedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstAlphabetizedLabel.Location = new System.Drawing.Point(161, 65);
+            this.firstAlphabetizedLabel.Name = "firstAlphabetizedLabel";
+            this.firstAlphabetizedLabel.Size = new System.Drawing.Size(300, 75);
+            this.firstAlphabetizedLabel.TabIndex = 2;
+            this.firstAlphabetizedLabel.Text = "label2";
+            // 
+            // lastAlphabetizedLabel
+            // 
+            this.lastAlphabetizedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastAlphabetizedLabel.Location = new System.Drawing.Point(161, 140);
+            this.lastAlphabetizedLabel.Name = "lastAlphabetizedLabel";
+            this.lastAlphabetizedLabel.Size = new System.Drawing.Size(300, 75);
+            this.lastAlphabetizedLabel.TabIndex = 3;
+            this.lastAlphabetizedLabel.Text = "label3";
+            // 
+            // longestWordLabel
+            // 
+            this.longestWordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.longestWordLabel.Location = new System.Drawing.Point(161, 215);
+            this.longestWordLabel.Name = "longestWordLabel";
+            this.longestWordLabel.Size = new System.Drawing.Size(300, 75);
+            this.longestWordLabel.TabIndex = 4;
+            this.longestWordLabel.Text = "label4";
+            // 
+            // mostVowelsLabel
+            // 
+            this.mostVowelsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mostVowelsLabel.Location = new System.Drawing.Point(161, 290);
+            this.mostVowelsLabel.Name = "mostVowelsLabel";
+            this.mostVowelsLabel.Size = new System.Drawing.Size(300, 75);
+            this.mostVowelsLabel.TabIndex = 5;
+            this.mostVowelsLabel.Text = "label5";
+            // 
+            // fileInputTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 417);
+            this.Controls.Add(this.mostVowelsLabel);
+            this.Controls.Add(this.longestWordLabel);
+            this.Controls.Add(this.lastAlphabetizedLabel);
+            this.Controls.Add(this.firstAlphabetizedLabel);
+            this.Controls.Add(this.wordListLabel);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "fileInputTestForm";
+            this.Text = "File Input Test";
+            this.Load += new System.EventHandler(this.fileInputTestForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openInputFileDialog;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Label wordListLabel;
+        private System.Windows.Forms.Label firstAlphabetizedLabel;
+        private System.Windows.Forms.Label lastAlphabetizedLabel;
+        private System.Windows.Forms.Label longestWordLabel;
+        private System.Windows.Forms.Label mostVowelsLabel;
+    }
+}
+
---
+++ Activity 5/Activity 5/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Activity_5
+{
+    public partial class fileInputTestForm : Form
+    {
+        public fileInputTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFromFile;
+            StreamWriter outputToFile;
+            openInputFileDialog.ShowDialog();
+            if (openInputFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                inputFromFile = File.OpenText(openInputFileDialog.FileName);
+                outputToFile = File.CreateText("Test Output.txt");
+                string dataFromFile = "";
+
+                string wordList = "";
+
+                string firstAlphabetizedWord = "";
+                string lastAlphabetizedWord = "";
+                string longestWord = "";
+
+                int mostVowelsInMostVowelWord = 0;
+                int vowelCount;
+                string mostvowelWord = "";
+
+                while (!inputFromFile.EndOfStream)
+                {
+                    //Obtain a line from the input file and convert it to lower case
+                    dataFromFile = inputFromFile.ReadLine().ToLower();
+                    //Show the line
+
+                    wordList = (wordList + Environment.NewLine + dataFromFile);
+                    wordListLabel.Text = wordList;
+
+                    //Comparison tests
+                    if (firstAlphabetizedWord == "")
+                    {
+                        firstAlphabetizedWord = dataFromFile;
+                    }
+                    else if (string.Compare(dataFromFile, firstAlphabetizedWord) == -1)
+                    {
+                        firstAlphabetizedWord = dataFromFile;
+                    }
+                    if (lastAlphabetizedWord == "")
+                    {
+                        lastAlphabetizedWord = dataFromFile;
+                    }
+                    else if (string.Compare(dataFromFile, lastAlphabetizedWord) == 1)
+                    {
+                        lastAlphabetizedWord = dataFromFile;
+                    }
+                    if (longestWord == "")
+                    {
+                        longestWord = dataFromFile;
+                    }
+                    else if (dataFromFile.Length > longestWord.Length)
+                    {
+                        longestWord = dataFromFile;
+                    }
+                    vowelCount = 0;
+                    for (int i = 0; i < dataFromFile.Length; i++)
+                    {
+                        if (dataFromFile[i] == 'a' || dataFromFile[i] == 'e' ||
+                            dataFromFile[i] == 'i' || dataFromFile[i] == 'o' ||
+                            dataFromFile[i] == 'u')
+                        {
+                            vowelCount++;
+                        }
+                    }
+                    if (mostVowelsInMostVowelWord == 0)
+                    {
+                        mostVowelsInMostVowelWord = vowelCount;
+                        mostvowelWord = dataFromFile;
+                    }
+                    else if (vowelCount > mostVowelsInMostVowelWord)
+                    {
+                        mostVowelsInMostVowelWord = vowelCount;
+                        mostvowelWord = dataFromFile;
+                    }
+
+
+                    //Output the lowercase string to a file
+                    outputToFile.WriteLine(dataFromFile);
+                }
+
+                //Show comparison results
+                firstAlphabetizedLabel.Text = ("First Alphabetically: " + firstAlphabetizedWord);
+                lastAlphabetizedLabel.Text = ("Last Alphabetically: " + lastAlphabetizedWord);
+                longestWordLabel.Text = ("Longest Word: " + longestWord);
+                mostVowelsLabel.Text = ("Most Vowels: " + mostvowelWord);
+
+                //Output Comparison Results
+                outputToFile.WriteLine("First Alphabetically: " + firstAlphabetizedWord);
+                outputToFile.WriteLine("Last Alphabetically: " + lastAlphabetizedWord);
+                outputToFile.WriteLine("Longest Word: " + longestWord);
+                outputToFile.WriteLine("Most Vowels: " + mostvowelWord);
+
+                //Close files
+                inputFromFile.Close();
+                outputToFile.Close();
+            }
+            else
+            {
+                MessageBox.Show("Canceled File Load");
+            }
+        }
+
+        private void fileInputTestForm_Load(object sender, EventArgs e)
+        {
+            //Clear Labels on Load
+            wordListLabel.Text = "";
+            firstAlphabetizedLabel.Text = "";
+            lastAlphabetizedLabel.Text = "";
+            longestWordLabel.Text = "";
+            mostVowelsLabel.Text = "";
+        }
+    }
+}
