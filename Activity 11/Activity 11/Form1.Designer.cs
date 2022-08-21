2eb45424a238ec02ac7effaac3dd2e54b7cb97dd
Christa Moncrief <109888173+ScribeEzra@users.noreply.github.com>
2022-08-21 18:48:25 -0500

    Add files via upload

---
+++ Activity 11/Activity 11/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Activity_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.die1Display = new System.Windows.Forms.Label();
+            this.die2Display = new System.Windows.Forms.Label();
+            this.die1Label = new System.Windows.Forms.Label();
+            this.die2Label = new System.Windows.Forms.Label();
+            this.sidesLabel = new System.Windows.Forms.Label();
+            this.diceSidesInput = new System.Windows.Forms.TextBox();
+            this.rollDieButton = new System.Windows.Forms.Button();
+            this.diceRollLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // die1Display
+            // 
+            this.die1Display.BackColor = System.Drawing.Color.White;
+            this.die1Display.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.die1Display.Font = new System.Drawing.Font("Felix Titling", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.die1Display.Location = new System.Drawing.Point(30, 63);
+            this.die1Display.Name = "die1Display";
+            this.die1Display.Size = new System.Drawing.Size(150, 150);
+            this.die1Display.TabIndex = 0;
+            this.die1Display.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // die2Display
+            // 
+            this.die2Display.BackColor = System.Drawing.Color.White;
+            this.die2Display.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.die2Display.Font = new System.Drawing.Font("Felix Titling", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.die2Display.Location = new System.Drawing.Point(216, 63);
+            this.die2Display.Name = "die2Display";
+            this.die2Display.Size = new System.Drawing.Size(150, 150);
+            this.die2Display.TabIndex = 1;
+            this.die2Display.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // die1Label
+            // 
+            this.die1Label.AutoSize = true;
+            this.die1Label.BackColor = System.Drawing.Color.Transparent;
+            this.die1Label.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.die1Label.ForeColor = System.Drawing.Color.Snow;
+            this.die1Label.Location = new System.Drawing.Point(62, 24);
+            this.die1Label.Name = "die1Label";
+            this.die1Label.Size = new System.Drawing.Size(90, 38);
+            this.die1Label.TabIndex = 2;
+            this.die1Label.Text = "Die 1";
+            // 
+            // die2Label
+            // 
+            this.die2Label.AutoSize = true;
+            this.die2Label.BackColor = System.Drawing.Color.Transparent;
+            this.die2Label.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.die2Label.ForeColor = System.Drawing.Color.Snow;
+            this.die2Label.Location = new System.Drawing.Point(245, 24);
+            this.die2Label.Name = "die2Label";
+            this.die2Label.Size = new System.Drawing.Size(90, 38);
+            this.die2Label.TabIndex = 3;
+            this.die2Label.Text = "Die 2";
+            // 
+            // sidesLabel
+            // 
+            this.sidesLabel.AutoSize = true;
+            this.sidesLabel.BackColor = System.Drawing.Color.Transparent;
+            this.sidesLabel.Font = new System.Drawing.Font("Franklin Gothic Book", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sidesLabel.ForeColor = System.Drawing.Color.Snow;
+            this.sidesLabel.Location = new System.Drawing.Point(388, 63);
+            this.sidesLabel.Name = "sidesLabel";
+            this.sidesLabel.Size = new System.Drawing.Size(143, 29);
+            this.sidesLabel.TabIndex = 4;
+            this.sidesLabel.Text = "Sides (4-20)";
+            // 
+            // diceSidesInput
+            // 
+            this.diceSidesInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diceSidesInput.Location = new System.Drawing.Point(393, 104);
+            this.diceSidesInput.Name = "diceSidesInput";
+            this.diceSidesInput.Size = new System.Drawing.Size(138, 27);
+            this.diceSidesInput.TabIndex = 5;
+            // 
+            // rollDieButton
+            // 
+            this.rollDieButton.Font = new System.Drawing.Font("Franklin Gothic Book", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rollDieButton.Location = new System.Drawing.Point(393, 159);
+            this.rollDieButton.Name = "rollDieButton";
+            this.rollDieButton.Size = new System.Drawing.Size(138, 54);
+            this.rollDieButton.TabIndex = 6;
+            this.rollDieButton.Text = "Roll Dice";
+            this.rollDieButton.UseVisualStyleBackColor = true;
+            this.rollDieButton.Click += new System.EventHandler(this.rollDieButton_Click);
+            // 
+            // diceRollLog
+            // 
+            this.diceRollLog.BackColor = System.Drawing.Color.Black;
+            this.diceRollLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diceRollLog.ForeColor = System.Drawing.Color.White;
+            this.diceRollLog.FormattingEnabled = true;
+            this.diceRollLog.ItemHeight = 20;
+            this.diceRollLog.Location = new System.Drawing.Point(30, 243);
+            this.diceRollLog.Name = "diceRollLog";
+            this.diceRollLog.ScrollAlwaysVisible = true;
+            this.diceRollLog.Size = new System.Drawing.Size(499, 104);
+            this.diceRollLog.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(570, 391);
+            this.Controls.Add(this.diceRollLog);
+            this.Controls.Add(this.rollDieButton);
+            this.Controls.Add(this.diceSidesInput);
+            this.Controls.Add(this.sidesLabel);
+            this.Controls.Add(this.die2Label);
+            this.Controls.Add(this.die1Label);
+            this.Controls.Add(this.die2Display);
+            this.Controls.Add(this.die1Display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label die1Display;
+        private System.Windows.Forms.Label die2Display;
+        private System.Windows.Forms.Label die1Label;
+        private System.Windows.Forms.Label die2Label;
+        private System.Windows.Forms.Label sidesLabel;
+        private System.Windows.Forms.TextBox diceSidesInput;
+        private System.Windows.Forms.Button rollDieButton;
+        private System.Windows.Forms.ListBox diceRollLog;
+    }
+}
+
---
+++ Activity 11/Activity 11/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rollDieButton_Click(object sender, EventArgs e)
+        {
+            int diceSides;
+            if (int.TryParse(diceSidesInput.Text, out diceSides))
+            {
+                if (!(diceSides >= 4) && !(diceSides <= 20))
+                {
+                    MessageBox.Show("Please choose a number from 4 through 20.");
+                }
+                else
+                {
+                    SnakeEyesRoll(diceSides);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input");
+            }
+        }
+        public class Die
+        {
+            private int numDieSides;
+
+            public Die()
+            {
+                //creates a 6-sided die if no arguements
+                numDieSides = 6;
+            }
+            public Die(int sides)
+            {
+                //Sets sides to user input
+                numDieSides = sides;
+            }
+            public int RollDie(Die die)
+            {
+                //a Random object must be created first
+                Random randVal = new Random();
+                //this statement generates a number betwen 1 and the number of sides
+                int dieVal = randVal.Next(1, die.numDieSides);
+                //the random value is returned
+                return dieVal;
+            }
+            
+        }
+        public void SnakeEyesRoll(int sides)
+        {
+            //this variable keeps track of the number of rolls
+            int numRolls = 0;
+
+            //these variables keep track of the die values
+            //They can't start null due to the upcoming while loop so they start with values of -1
+            int die1Val = -1;
+            int die2Val = -1;
+
+            //this defines two dice
+            Die die1 = new Die(sides);
+            Die die2 = new Die(sides);
+
+            //clearing the information from the last run
+            ClearDiceLog();
+
+            string rollInfo = "Rolling for snake eyes...";
+            LogDiceRoll(rollInfo);
+
+
+            while (!(die1Val == 1 && die2Val == 1))
+            {
+                //add to number of rolls
+                numRolls++;
+
+                //roll both die to get values
+                die1Val = die1.RollDie(die1);
+                //Add ten millisecond delay between rolls as otherwise the pseudorandom numbers
+                //are the same each time
+                Thread.Sleep(10);
+                die2Val = die2.RollDie(die2);
+
+                //for some reason I couldn't get this to display the values until after the SnakeEyesRoll method
+                //was over, even when adding a 1/4 second delay
+                DisplayDice(die1Val, die2Val);
+
+                rollInfo = "Roll " + numRolls + " - Die 1: " + die1Val + " Die 2: " + die2Val;
+                LogDiceRoll(rollInfo);
+                //pause for half of a second for effect
+                //(to show the dice changing and the log filling up at high speed)
+                //Thread.Sleep(250);
+            }
+            MessageBox.Show("It took " + numRolls + " rolls to roll snake eyes!");
+        }
+        public void LogDiceRoll(string log)
+        {
+            //adds info of dice roll to listbox
+            diceRollLog.Items.Add(log);
+        }
+        public void DisplayDice(int val1, int val2)
+        {
+            //displays dice values
+            die1Display.Text = val1.ToString();
+            die2Display.Text = val2.ToString();
+        }
+        public void ClearDiceLog()
+        {
+            //clears listbox
+            diceRollLog.Items.Clear();
+        }
+    }
+}
