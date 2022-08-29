582c2f89d11d8554eacaf26c39ce063bb25fcc0f
Christa Moncrief <109888173+ScribeEzra@users.noreply.github.com>
2022-08-28 21:58:43 -0500

    Add files via upload

---
+++ Activity 14/Activity 14/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace Activity_14
+{
+    partial class tacoCentral
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.tortillaBox = new System.Windows.Forms.GroupBox();
+            this.cTortillaRadio = new System.Windows.Forms.RadioButton();
+            this.fTortillaRadio = new System.Windows.Forms.RadioButton();
+            this.ingredientBox = new System.Windows.Forms.GroupBox();
+            this.beefCheck = new System.Windows.Forms.CheckBox();
+            this.beansCheck = new System.Windows.Forms.CheckBox();
+            this.chickenCheck = new System.Windows.Forms.CheckBox();
+            this.tomatoCheck = new System.Windows.Forms.CheckBox();
+            this.lettuceCheck = new System.Windows.Forms.CheckBox();
+            this.onionCheck = new System.Windows.Forms.CheckBox();
+            this.cheeseCheck = new System.Windows.Forms.CheckBox();
+            this.avocadoCheck = new System.Windows.Forms.CheckBox();
+            this.sourCreamCheck = new System.Windows.Forms.CheckBox();
+            this.sauceBox = new System.Windows.Forms.GroupBox();
+            this.salsaCheck = new System.Windows.Forms.CheckBox();
+            this.quesoCheck = new System.Windows.Forms.CheckBox();
+            this.picoCheck = new System.Windows.Forms.CheckBox();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.orderDisplay = new System.Windows.Forms.Label();
+            this.jalapenoCheck = new System.Windows.Forms.CheckBox();
+            this.cilantroCheck = new System.Windows.Forms.CheckBox();
+            this.tortillaBox.SuspendLayout();
+            this.ingredientBox.SuspendLayout();
+            this.sauceBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Informal Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(139, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(209, 44);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Taco Central";
+            // 
+            // tortillaBox
+            // 
+            this.tortillaBox.Controls.Add(this.fTortillaRadio);
+            this.tortillaBox.Controls.Add(this.cTortillaRadio);
+            this.tortillaBox.Location = new System.Drawing.Point(50, 74);
+            this.tortillaBox.Name = "tortillaBox";
+            this.tortillaBox.Size = new System.Drawing.Size(382, 59);
+            this.tortillaBox.TabIndex = 1;
+            this.tortillaBox.TabStop = false;
+            this.tortillaBox.Text = "groupBox1";
+            // 
+            // cTortillaRadio
+            // 
+            this.cTortillaRadio.AutoSize = true;
+            this.cTortillaRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cTortillaRadio.Location = new System.Drawing.Point(27, 21);
+            this.cTortillaRadio.Name = "cTortillaRadio";
+            this.cTortillaRadio.Size = new System.Drawing.Size(113, 20);
+            this.cTortillaRadio.TabIndex = 0;
+            this.cTortillaRadio.TabStop = true;
+            this.cTortillaRadio.Text = "Corn Tortilla";
+            this.cTortillaRadio.UseVisualStyleBackColor = true;
+            // 
+            // fTortillaRadio
+            // 
+            this.fTortillaRadio.AutoSize = true;
+            this.fTortillaRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fTortillaRadio.Location = new System.Drawing.Point(235, 21);
+            this.fTortillaRadio.Name = "fTortillaRadio";
+            this.fTortillaRadio.Size = new System.Drawing.Size(116, 20);
+            this.fTortillaRadio.TabIndex = 2;
+            this.fTortillaRadio.TabStop = true;
+            this.fTortillaRadio.Text = "Flour Tortilla";
+            this.fTortillaRadio.UseVisualStyleBackColor = true;
+            // 
+            // ingredientBox
+            // 
+            this.ingredientBox.Controls.Add(this.cilantroCheck);
+            this.ingredientBox.Controls.Add(this.jalapenoCheck);
+            this.ingredientBox.Controls.Add(this.sourCreamCheck);
+            this.ingredientBox.Controls.Add(this.avocadoCheck);
+            this.ingredientBox.Controls.Add(this.cheeseCheck);
+            this.ingredientBox.Controls.Add(this.onionCheck);
+            this.ingredientBox.Controls.Add(this.lettuceCheck);
+            this.ingredientBox.Controls.Add(this.tomatoCheck);
+            this.ingredientBox.Controls.Add(this.chickenCheck);
+            this.ingredientBox.Controls.Add(this.beansCheck);
+            this.ingredientBox.Controls.Add(this.beefCheck);
+            this.ingredientBox.Location = new System.Drawing.Point(50, 150);
+            this.ingredientBox.Name = "ingredientBox";
+            this.ingredientBox.Size = new System.Drawing.Size(382, 134);
+            this.ingredientBox.TabIndex = 3;
+            this.ingredientBox.TabStop = false;
+            this.ingredientBox.Text = "groupBox2";
+            // 
+            // beefCheck
+            // 
+            this.beefCheck.AutoSize = true;
+            this.beefCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beefCheck.Location = new System.Drawing.Point(27, 21);
+            this.beefCheck.Name = "beefCheck";
+            this.beefCheck.Size = new System.Drawing.Size(61, 20);
+            this.beefCheck.TabIndex = 0;
+            this.beefCheck.Text = "Beef";
+            this.beefCheck.UseVisualStyleBackColor = true;
+            // 
+            // beansCheck
+            // 
+            this.beansCheck.AutoSize = true;
+            this.beansCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beansCheck.Location = new System.Drawing.Point(147, 21);
+            this.beansCheck.Name = "beansCheck";
+            this.beansCheck.Size = new System.Drawing.Size(73, 20);
+            this.beansCheck.TabIndex = 1;
+            this.beansCheck.Text = "Beans";
+            this.beansCheck.UseVisualStyleBackColor = true;
+            // 
+            // chickenCheck
+            // 
+            this.chickenCheck.AutoSize = true;
+            this.chickenCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chickenCheck.Location = new System.Drawing.Point(266, 21);
+            this.chickenCheck.Name = "chickenCheck";
+            this.chickenCheck.Size = new System.Drawing.Size(84, 20);
+            this.chickenCheck.TabIndex = 2;
+            this.chickenCheck.Text = "Chicken";
+            this.chickenCheck.UseVisualStyleBackColor = true;
+            // 
+            // tomatoCheck
+            // 
+            this.tomatoCheck.AutoSize = true;
+            this.tomatoCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tomatoCheck.Location = new System.Drawing.Point(27, 47);
+            this.tomatoCheck.Name = "tomatoCheck";
+            this.tomatoCheck.Size = new System.Drawing.Size(82, 20);
+            this.tomatoCheck.TabIndex = 3;
+            this.tomatoCheck.Text = "Tomato";
+            this.tomatoCheck.UseVisualStyleBackColor = true;
+            // 
+            // lettuceCheck
+            // 
+            this.lettuceCheck.AutoSize = true;
+            this.lettuceCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lettuceCheck.Location = new System.Drawing.Point(147, 47);
+            this.lettuceCheck.Name = "lettuceCheck";
+            this.lettuceCheck.Size = new System.Drawing.Size(79, 20);
+            this.lettuceCheck.TabIndex = 4;
+            this.lettuceCheck.Text = "Lettuce";
+            this.lettuceCheck.UseVisualStyleBackColor = true;
+            // 
+            // onionCheck
+            // 
+            this.onionCheck.AutoSize = true;
+            this.onionCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onionCheck.Location = new System.Drawing.Point(266, 47);
+            this.onionCheck.Name = "onionCheck";
+            this.onionCheck.Size = new System.Drawing.Size(77, 20);
+            this.onionCheck.TabIndex = 5;
+            this.onionCheck.Text = "Onions";
+            this.onionCheck.UseVisualStyleBackColor = true;
+            // 
+            // cheeseCheck
+            // 
+            this.cheeseCheck.AutoSize = true;
+            this.cheeseCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cheeseCheck.Location = new System.Drawing.Point(27, 73);
+            this.cheeseCheck.Name = "cheeseCheck";
+            this.cheeseCheck.Size = new System.Drawing.Size(82, 20);
+            this.cheeseCheck.TabIndex = 4;
+            this.cheeseCheck.Text = "Cheese";
+            this.cheeseCheck.UseVisualStyleBackColor = true;
+            // 
+            // avocadoCheck
+            // 
+            this.avocadoCheck.AutoSize = true;
+            this.avocadoCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.avocadoCheck.Location = new System.Drawing.Point(147, 73);
+            this.avocadoCheck.Name = "avocadoCheck";
+            this.avocadoCheck.Size = new System.Drawing.Size(91, 20);
+            this.avocadoCheck.TabIndex = 4;
+            this.avocadoCheck.Text = "Avocado";
+            this.avocadoCheck.UseVisualStyleBackColor = true;
+            // 
+            // sourCreamCheck
+            // 
+            this.sourCreamCheck.AutoSize = true;
+            this.sourCreamCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sourCreamCheck.Location = new System.Drawing.Point(266, 73);
+            this.sourCreamCheck.Name = "sourCreamCheck";
+            this.sourCreamCheck.Size = new System.Drawing.Size(110, 20);
+            this.sourCreamCheck.TabIndex = 4;
+            this.sourCreamCheck.Text = "Sour Cream";
+            this.sourCreamCheck.UseVisualStyleBackColor = true;
+            // 
+            // sauceBox
+            // 
+            this.sauceBox.Controls.Add(this.picoCheck);
+            this.sauceBox.Controls.Add(this.quesoCheck);
+            this.sauceBox.Controls.Add(this.salsaCheck);
+            this.sauceBox.Location = new System.Drawing.Point(50, 300);
+            this.sauceBox.Name = "sauceBox";
+            this.sauceBox.Size = new System.Drawing.Size(382, 61);
+            this.sauceBox.TabIndex = 4;
+            this.sauceBox.TabStop = false;
+            this.sauceBox.Text = "groupBox3";
+            // 
+            // salsaCheck
+            // 
+            this.salsaCheck.AutoSize = true;
+            this.salsaCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salsaCheck.Location = new System.Drawing.Point(27, 21);
+            this.salsaCheck.Name = "salsaCheck";
+            this.salsaCheck.Size = new System.Drawing.Size(69, 20);
+            this.salsaCheck.TabIndex = 5;
+            this.salsaCheck.Text = "Salsa";
+            this.salsaCheck.UseVisualStyleBackColor = true;
+            // 
+            // quesoCheck
+            // 
+            this.quesoCheck.AutoSize = true;
+            this.quesoCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quesoCheck.Location = new System.Drawing.Point(150, 20);
+            this.quesoCheck.Name = "quesoCheck";
+            this.quesoCheck.Size = new System.Drawing.Size(74, 20);
+            this.quesoCheck.TabIndex = 6;
+            this.quesoCheck.Text = "Queso";
+            this.quesoCheck.UseVisualStyleBackColor = true;
+            // 
+            // picoCheck
+            // 
+            this.picoCheck.AutoSize = true;
+            this.picoCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.picoCheck.Location = new System.Drawing.Point(269, 21);
+            this.picoCheck.Name = "picoCheck";
+            this.picoCheck.Size = new System.Drawing.Size(60, 20);
+            this.picoCheck.TabIndex = 7;
+            this.picoCheck.Text = "Pico";
+            this.picoCheck.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.picoCheck.UseVisualStyleBackColor = true;
+            // 
+            // orderButton
+            // 
+            this.orderButton.BackColor = System.Drawing.Color.Black;
+            this.orderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderButton.ForeColor = System.Drawing.Color.White;
+            this.orderButton.Location = new System.Drawing.Point(157, 468);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(157, 29);
+            this.orderButton.TabIndex = 5;
+            this.orderButton.Text = "Order";
+            this.orderButton.UseVisualStyleBackColor = false;
+            this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
+            // 
+            // orderDisplay
+            // 
+            this.orderDisplay.BackColor = System.Drawing.Color.White;
+            this.orderDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderDisplay.Location = new System.Drawing.Point(75, 380);
+            this.orderDisplay.Name = "orderDisplay";
+            this.orderDisplay.Size = new System.Drawing.Size(326, 73);
+            this.orderDisplay.TabIndex = 6;
+            // 
+            // jalapenoCheck
+            // 
+            this.jalapenoCheck.AutoSize = true;
+            this.jalapenoCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jalapenoCheck.Location = new System.Drawing.Point(27, 99);
+            this.jalapenoCheck.Name = "jalapenoCheck";
+            this.jalapenoCheck.Size = new System.Drawing.Size(99, 20);
+            this.jalapenoCheck.TabIndex = 7;
+            this.jalapenoCheck.Text = "Japapeno";
+            this.jalapenoCheck.UseVisualStyleBackColor = true;
+            // 
+            // cilantroCheck
+            // 
+            this.cilantroCheck.AutoSize = true;
+            this.cilantroCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cilantroCheck.Location = new System.Drawing.Point(147, 99);
+            this.cilantroCheck.Name = "cilantroCheck";
+            this.cilantroCheck.Size = new System.Drawing.Size(82, 20);
+            this.cilantroCheck.TabIndex = 8;
+            this.cilantroCheck.Text = "Cilantro";
+            this.cilantroCheck.UseVisualStyleBackColor = true;
+            // 
+            // tacoCentral
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(474, 509);
+            this.Controls.Add(this.orderDisplay);
+            this.Controls.Add(this.orderButton);
+            this.Controls.Add(this.sauceBox);
+            this.Controls.Add(this.ingredientBox);
+            this.Controls.Add(this.tortillaBox);
+            this.Controls.Add(this.titleLabel);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "tacoCentral";
+            this.Text = "Taco Central";
+            this.tortillaBox.ResumeLayout(false);
+            this.tortillaBox.PerformLayout();
+            this.ingredientBox.ResumeLayout(false);
+            this.ingredientBox.PerformLayout();
+            this.sauceBox.ResumeLayout(false);
+            this.sauceBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.GroupBox tortillaBox;
+        private System.Windows.Forms.RadioButton fTortillaRadio;
+        private System.Windows.Forms.RadioButton cTortillaRadio;
+        private System.Windows.Forms.GroupBox ingredientBox;
+        private System.Windows.Forms.CheckBox sourCreamCheck;
+        private System.Windows.Forms.CheckBox avocadoCheck;
+        private System.Windows.Forms.CheckBox cheeseCheck;
+        private System.Windows.Forms.CheckBox onionCheck;
+        private System.Windows.Forms.CheckBox lettuceCheck;
+        private System.Windows.Forms.CheckBox tomatoCheck;
+        private System.Windows.Forms.CheckBox chickenCheck;
+        private System.Windows.Forms.CheckBox beansCheck;
+        private System.Windows.Forms.CheckBox beefCheck;
+        private System.Windows.Forms.GroupBox sauceBox;
+        private System.Windows.Forms.CheckBox picoCheck;
+        private System.Windows.Forms.CheckBox quesoCheck;
+        private System.Windows.Forms.CheckBox salsaCheck;
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.Label orderDisplay;
+        private System.Windows.Forms.CheckBox cilantroCheck;
+        private System.Windows.Forms.CheckBox jalapenoCheck;
+    }
+}
+
---
+++ Activity 14/Activity 14/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_14
+{
+    public partial class tacoCentral : Form
+    {
+        public tacoCentral()
+        {
+            InitializeComponent();
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)
+        {
+            //create a blank string
+            string order = "";
+            //add checked items to string
+            if (cTortillaRadio.Checked)
+            {
+                order += "Corn Tortilla";
+            }
+            else
+            {
+                order += "Flour Tortilla";
+            }
+            if (beefCheck.Checked)
+            {
+                order += ", Beef";
+            }
+            if (beansCheck.Checked)
+            {
+                order += ", Beans";
+            }
+            if (chickenCheck.Checked)
+            {
+                order += ", Chicken";
+            }
+            if (tomatoCheck.Checked)
+            {
+                order += ", Tomato";
+            }
+            if (lettuceCheck.Checked)
+            {
+                order += ", Lettuce";
+            }
+            if (onionCheck.Checked)
+            {
+                order += ", Onion";
+            }
+            if (cheeseCheck.Checked)
+            {
+                order += ", Cheese";
+            }
+            if (avocadoCheck.Checked)
+            {
+                order += ", Avocado";
+            }
+            if (sourCreamCheck.Checked)
+            {
+                order += ", Sour Cream";
+            }
+            if (jalapenoCheck.Checked)
+            {
+                order += ", Japapeno ";
+            }
+            if (cilantroCheck.Checked)
+            {
+                order += ", Cilantro ";
+            }
+            if (salsaCheck.Checked)
+            {
+                order += ", Salsa ";
+            }
+            if (quesoCheck.Checked)
+            {
+                order += ", Queso ";
+            }
+            if (picoCheck.Checked)
+            {
+                order += ", Pico ";
+            }
+            //display order selections
+            orderDisplay.Text = order;
+        }
+    }
+}
